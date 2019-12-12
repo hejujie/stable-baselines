2732a0fa437ee172706fdcc6f076c543ba797919
Antonin RAFFIN <antonin.raffin@ensta.org>
2019-12-12 13:52:46 +0100

    Test common failures

--- stable_baselines/common/env_checker.py
+++ stable_baselines/common/env_checker.py
@@ -4,6 +4,8 @@ import gym
 from gym import spaces
 import numpy as np
 
+from stable_baselines.common.vec_env import DummyVecEnv, VecCheckNan
+
 
 def check_env(env, warn=True):
     """
@@ -16,7 +18,6 @@ def check_env(env, warn=True):
     :param warn: (bool) Whether to output additional warnings
         mainly related to the interaction with Stable Baselines
     """
-
     # Helper to link to the code, because gym has no proper documentation
     gym_env = " cf https://github.com/openai/gym/blob/master/gym/core.py"
     gym_spaces = " cf https://github.com/openai/gym/blob/master/gym/spaces/"
@@ -31,6 +32,9 @@ def check_env(env, warn=True):
 
     observation_space = env.observation_space
     action_space = env.action_space
+    # Whether to check that the returned observation is a numpy array
+    # it is not mandatory for `Dict` and `Tuple` spaces
+    enforce_array_obs = not isinstance(observation_space, (spaces.Dict, spaces.Tuple))
 
     assert isinstance(observation_space, spaces.Space), "The observation space must inherit from gym.spaces" + gym_spaces
     assert isinstance(action_space, spaces.Space), "The action space must inherit from gym.spaces" + gym_spaces
@@ -71,23 +75,34 @@ def check_env(env, warn=True):
                           "You might need to use a custom `cnn_extractor` "
                           "cf https://stable-baselines.readthedocs.io/en/master/guide/custom_policy.html")
 
+    if warn and isinstance(observation_space, spaces.Box) and len(observation_space.shape) not in [1, 3]:
+        warnings.warn("Your observation has not a conventional shape (neither an image, nor a 1D vector). "
+                      "We recommend you to flatten the observation "
+                      "to have only a 1D vector")
 
     if warn and isinstance(action_space, spaces.Box):
         # if the if-block is not separated `action_space.low` will be evaluated even though
         # `isinstance(action_space, spaces.Box)` is False...
-        if action_space.low != action_space.high or np.abs(action_space.low) > 1 or np.abs(action_space.high) > 1:
-            warnings.warn("We recommend you to use a symmetric and normalized Box action space (range=[-1, 1])"
+        if np.abs(action_space.low) != np.abs(action_space.high) or np.abs(action_space.low) > 1 or np.abs(action_space.high) > 1:
+            warnings.warn("We recommend you to use a symmetric and normalized Box action space (range=[-1, 1]) "
                           "cf https://stable-baselines.readthedocs.io/en/master/guide/rl_tips.html")
 
 
     # ============ Check the returned values ===============
-    # because env inherits from gym.Env, we assume that
+
+    # because env inherits from gym.Env, we assume that `reset()` and `step()` methods exists
     obs = env.reset()
 
-    assert not isinstance(obs, tuple), "The `reset()` method should only return one value, not a tuple"
+    if not isinstance(observation_space, spaces.Tuple):
+        assert not isinstance(obs, tuple), "The `reset()` method should only return one value, not a tuple"
+
     # The check for a GoalEnv is done by the base class
     if not is_goal_env:
-        assert isinstance(obs, np.ndarray), "The observation returned by `reset()` method must be a numpy array"
+        if isinstance(observation_space, spaces.Discrete):
+            assert isinstance(obs, int), "The observation returned by `step()` method must be an int"
+        elif enforce_array_obs:
+            assert isinstance(obs, np.ndarray), "The observation returned by `reset()` method must be a numpy array"
+
     assert observation_space.contains(obs), "The observation returned by the `reset()` method does not match the given observation space"
 
     # Sample a random action
@@ -100,7 +115,10 @@ def check_env(env, warn=True):
     obs, reward, done, info = data
 
     if not is_goal_env:
-        assert isinstance(obs, np.ndarray), "The observation returned by `step()` method must be a numpy array"
+        if isinstance(observation_space, spaces.Discrete):
+            assert isinstance(obs, int), "The observation returned by `step()` method must be an int"
+        elif enforce_array_obs:
+            assert isinstance(obs, np.ndarray), "The observation returned by `step()` method must be a numpy array"
     assert observation_space.contains(obs), "The observation returned by the `step()` method does not match the given observation space"
 
     # We also allow int because the reward will be cast to float
@@ -112,5 +130,13 @@ def check_env(env, warn=True):
         # For a GoalEnv, the keys are check at reset
         assert reward == env.compute_reward(obs['achieved_goal'], obs['desired_goal'], info)
 
-    # TODO: check metadata['render.modes']
-    # Check for Inf and NaN using the VecWrapper?
+    # TODO: should we check metadata['render.modes'] ?
+
+    # Check for Inf and NaN using the VecWrapper
+    vec_env = VecCheckNan(DummyVecEnv([lambda: env]))
+
+    # The check only works with numpy arrays
+    if enforce_array_obs:
+        for _ in range(10):
+            action = [action_space.sample()]
+            _, _, _, _ = vec_env.step(action)
--- tests/test_envs.py
+++ tests/test_envs.py
@@ -1,8 +1,11 @@
 import pytest
 import gym
+from gym import spaces
+import numpy as np
 
 from stable_baselines.common.env_checker import check_env
 from stable_baselines.common.bit_flipping_env import BitFlippingEnv
+from stable_baselines.common.identity_env import IdentityEnv, IdentityEnvBox
 
 
 @pytest.mark.parametrize("env_id", ['CartPole-v0', 'Pendulum-v0', 'BreakoutNoFrameskip-v4'])
@@ -22,8 +25,79 @@ def test_env(env_id):
         assert len(record) == 1
 
 
-def test_custom_envs():
-    goal_env = BitFlippingEnv()
-    check_env(goal_env)
+@pytest.mark.parametrize("env_class", [IdentityEnv, IdentityEnvBox, BitFlippingEnv])
+def test_custom_envs(env_class):
+    env = env_class()
+    check_env(env)
 
-    # Check failures
+
+def test_failures_and_warnings():
+    """
+    Test that common failure cases are catched
+    """
+    env = gym.make('BreakoutNoFrameskip-v4')
+    # Change the observation space
+    non_default_spaces = [
+        # Small image
+        spaces.Box(low=0, high=255, shape=(32, 32, 3), dtype=np.uint8),
+        # Range not in [0, 255]
+        spaces.Box(low=0, high=1, shape=(64, 64, 3), dtype=np.uint8),
+        # Wrong dtype
+        spaces.Box(low=0, high=255, shape=(64, 64, 3), dtype=np.float32),
+        # Not an image, it should be a 1D vector
+        spaces.Box(low=-1, high=1, shape=(64, 3), dtype=np.float32),
+        # Tuple space is not supported by SB
+        spaces.Tuple([spaces.Discrete(5), spaces.Discrete(10)]),
+        # Dict space is not supported by SB when env is not a GoalEnv
+        spaces.Dict({"position": spaces.Discrete(5)}),
+    ]
+    for new_obs_space in non_default_spaces:
+        env.observation_space = new_obs_space
+        # Patch methods to avoid errors
+        env.reset = new_obs_space.sample
+        def patched_step(action):
+            return new_obs_space.sample(), 0.0, False, {}
+        env.step = patched_step
+        with pytest.warns(UserWarning):
+            check_env(env)
+
+    env = IdentityEnvBox()
+    # Return an observation that does not match the observation_space
+    def reset_wrong_shape():
+        return np.ones((3,))
+
+    env.reset = reset_wrong_shape
+    with pytest.raises(AssertionError):
+        check_env(env)
+
+    # Return not only the observation
+    def reset_tuple():
+        return env.observation_space.sample(), False
+
+    env.reset = reset_tuple
+    with pytest.raises(AssertionError):
+        check_env(env)
+
+    # Return a wrong reward
+    def step_wrong_reward(action):
+        return env.observation_space.sample(), np.ones(1), False, {}
+
+    env.step = step_wrong_reward
+    with pytest.raises(AssertionError):
+        check_env(env)
+
+    # Info dict is not returned
+    def step_no_info(action):
+        return env.observation_space.sample(), 0.0, False
+
+    env.step = step_no_info
+    with pytest.raises(AssertionError):
+        check_env(env)
+
+    # Done is not a boolean
+    def step_wrong_done(action):
+        return env.observation_space.sample(), 0.0, 3.0, False
+
+    env.step = step_wrong_done
+    with pytest.raises(AssertionError):
+        check_env(env)
